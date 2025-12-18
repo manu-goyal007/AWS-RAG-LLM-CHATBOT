c6c0bfd4f72f3349c00725d9b571577adf9d89bc
Manu Goyal <goyalm685@gmail.com>
2025-12-18 14:53:42 +0530

    third commit

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -2,7 +2,7 @@ resource "aws_rds_cluster" "aurora_serverless" {
   cluster_identifier      = var.cluster_identifier
   engine                  = "aurora-postgresql"
   engine_mode             = "provisioned"
-  engine_version          = var.engine_version
+  engine_version          = "15.6"
   database_name           = var.database_name
   master_username         = var.master_username
   master_password         = random_password.master_password.result
--- stack2/main.tf
+++ stack2/main.tf
@@ -5,18 +5,18 @@ provider "aws" {
 module "bedrock_kb" {
   source = "../modules/bedrock_kb" 
 
-  knowledge_base_name        = "my-bedrock-kb"
+  knowledge_base_name        = "my-bedrock-kb-manu"
   knowledge_base_description = "Knowledge base connected to Aurora Serverless database"
 
-  aurora_arn        = "" #TODO Update with output from stack1
+  aurora_arn        = "arn:aws:rds:us-east-1:549487857933:cluster:my-aurora-serverless" #TODO Update with output from stack1
   aurora_db_name    = "myapp"
-  aurora_endpoint   = "" # TODO Update with output from stack1
+  aurora_endpoint   = "my-aurora-serverless.cluster-cudwqetogsui.us-east-1.rds.amazonaws.com" # TODO Update with output from stack1
   aurora_table_name = "bedrock_integration.bedrock_kb"
   aurora_primary_key_field = "id"
   aurora_metadata_field = "metadata"
   aurora_text_field = "chunks"
   aurora_verctor_field = "embedding"
   aurora_username   = "dbadmin"
-  aurora_secret_arn = "" #TODO Update with output from stack1
-  s3_bucket_arn = "" #TODO Update with output from stack1
+  aurora_secret_arn = "arn:aws:secretsmanager:us-east-1:549487857933:secret:my-aurora-serverless-MyMMkT" #TODO Update with output from stack1
+  s3_bucket_arn = "arn:aws:s3:::manu-9941" #TODO Update with output from stack1
 }
